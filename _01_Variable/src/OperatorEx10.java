dcc47ab7a8b3907bf844b8c6b94a1819617f0a01
KimHyeonJun1 <167170882+KimHyeonJun1@users.noreply.github.com>
2024-05-02 17:34:50 +0900

    문장 변경
    OperatorEx10 문장 변경

--- _01_Variable/src/OperatorEx10.java
+++ _01_Variable/src/OperatorEx10.java
@@ -4,14 +4,14 @@ public class OperatorEx10 {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner( System.in);
-		System.out.println("나이를, 입력하세요");
+		System.out.println("당신의 나이는?");
 		
 		int age = scan.nextInt();
 	
 		String grade = age < 6 ? "유아"
 				: (age < 12  ? "소아"
 						: ( age < 19 ? "청소년" : "성인") ) ;
-				System.out.printf( "나이 : %d세 -> %s", age, grade);
+				System.out.printf( "%d세인 당신은 %s입니다", age, grade);
 				
 	}
 }
