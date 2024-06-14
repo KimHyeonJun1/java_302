f5776cf5bf15f40479465df74238415d67b6fadf
KimHyeonJun1 <167170882+KimHyeonJun1@users.noreply.github.com>
2024-06-14 17:21:47 +0900

    client,server공부
    송신,수신 공부 , Thread를 이용한 송신,수신 연습

---
+++ _12_TCP/src/thread/ClientMain.java
@@ -0,0 +1,28 @@
+package thread;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientMain {
+	public static void main(String[] args) {
+		Socket client = null;
+		
+		try {
+			client = new Socket("192.168.0.52", 8000);
+			System.out.println("서버에 연결됨");
+			
+			Thread receive = new ReceiveThread(client);
+			Thread send = new SendThread(client);
+			receive.start();
+			send.start();
+			
+			
+		} catch (UnknownHostException e) {
+		} catch (IOException e) {
+		}
+		
+		
+		
+	}
+}
